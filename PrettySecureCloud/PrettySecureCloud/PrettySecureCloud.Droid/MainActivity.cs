58c5eef4738000e57da3632c11c1a8ea83f5c9e8
zmartl <luca.marti@bluewin.ch>
2016-09-26 21:51:04 +0200

    App init

---
+++ PrettySecureCloud/PrettySecureCloud/PrettySecureCloud.Droid/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+[assembly: Application(Icon = "@drawable/Icon", Theme = "@android:style/Theme.Holo.Light")]
+
+namespace PrettySecureCloud.Droid
+{
+    [Activity(Label = "PrettySecureCloud", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
+
