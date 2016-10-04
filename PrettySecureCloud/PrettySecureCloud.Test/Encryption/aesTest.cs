400356b7c391cb2f09201d591d0bdbcbca1f0b31
Seraphin Elias Rihm <seraphin.rihm@edu.tbz.ch>
2016-10-04 16:38:59 +0200

    #added encryption and decryption

---
+++ PrettySecureCloud/PrettySecureCloud.Test/Encryption/aesTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PrettySecureCloud.Encryption;
+using PrettySecureCloud.Test.Infrastructure;
+
+namespace PrettySecureCloud.Test.Encryption
+{
+	class aesTest : IntegrationTestBase<AES>
+	{
+		protected override void DoSetup()
+		{
+			base.DoSetup();
+
+			UnitUnderTest = new AES();
+		}
+
+		[Test]
+		public void CanEncryptData()
+		{
+			//Arrange
+			var file = File.ReadAllBytes(@"C:\Users\Serafin\Source\Repos\PrettySecureCloud\PrettySecureCloud\PrettySecureCloud.Test\TestData\Test.txt");
+			//Act
+			var encrypted = UnitUnderTest.EncryptAes(file, new byte[16] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16});
+			//Assert
+			Assert.That(file, Is.Not.EqualTo(encrypted));
+		}
+	}
+}
---
+++ PrettySecureCloud/PrettySecureCloud/PrettySecureCloud/Encryption/AES.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using PCLCrypto;
+
+namespace PrettySecureCloud.Encryption
+{
+	public class AES : IAES
+	{
+		/// <summary>    
+		/// Encrypts given bytes using symmetric alogrithm AES    
+		/// </summary>    
+		/// <param name="data">data to encrypt</param>    
+		///^<param name="key">key to encryptr</param>  
+		/// <returns></returns>  
+		public byte[] EncryptAes(byte[] data, byte[] key)
+		{
+			ISymmetricKeyAlgorithmProvider aes = WinRTCrypto.SymmetricKeyAlgorithmProvider.OpenAlgorithm(SymmetricAlgorithm.AesCbcPkcs7);
+			ICryptographicKey symetricKey = aes.CreateSymmetricKey(key);
+			var bytes = WinRTCrypto.CryptographicEngine.Encrypt(symetricKey, data);
+			return bytes;
+		}
+
+		/// <summary>    
+		/// Decrypts given bytes using symmetric alogrithm AES    
+		/// </summary>    
+		/// <param name="data">data to decrypt</param>    
+		///<param name="key">key to decrypt</param>  
+		/// <returns></returns>    
+		public byte[] DecryptAes(byte[] data, byte[] key)
+		{
+
+			ISymmetricKeyAlgorithmProvider aes = WinRTCrypto.SymmetricKeyAlgorithmProvider.OpenAlgorithm(SymmetricAlgorithm.AesCbcPkcs7);
+			ICryptographicKey symetricKey = aes.CreateSymmetricKey(key);
+			var bytes = WinRTCrypto.CryptographicEngine.Decrypt(symetricKey, data);
+			return bytes;
+		}
+	}
+}
