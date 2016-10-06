3214b9742a831c1f684983ec5461a6fafcbc81e8
Pascal Honegger <pascal.honegger.external@atos.net>
2016-10-06 10:26:36 +0200

    Xamarin-Icon mit unserem Icon ersetzt

--- PrettySecureCloud/PrettySecureCloud/PrettySecureCloud/Properties/AssemblyInfo.cs
+++ PrettySecureCloud/PrettySecureCloud/PrettySecureCloud/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PrettySecureCloud")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
