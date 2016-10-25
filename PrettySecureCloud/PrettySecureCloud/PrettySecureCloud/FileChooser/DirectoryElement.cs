59bb02bd14080d19826dbbc51f39955723e7c20a
lordmcfungus <alain.keller@edu.tbz.ch>
2016-10-25 11:18:50 +0200

    Bitzli Filechosser

---
+++ PrettySecureCloud/PrettySecureCloud/PrettySecureCloud/FileChooser/DirectoryElement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PrettySecureCloud.FileChooser
+{
+	class DirectoryElement
+	{
+		public string FileName { get; set; }
+		public ImageSource image { get; set; }
+	}
+}
