8d16d15bad263615f7993b612be4f8b19df6b5a4
lordmcfungus <alain.keller@edu.tbz.ch>
2016-10-25 10:38:54 +0200

    Icons hinzugefügt

---
+++ PrettySecureCloud/PrettySecureCloud/PrettySecureCloud/Pages/FileChooserPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace PrettySecureCloud.Pages
+{
+	public partial class FileChooserPage : ContentPage
+	{
+		public FileChooserPage()
+		{
+			InitializeComponent();
+
+			BindingContext = new FileChooserViewModel();
+		}
+
+		private void OnSelection(object sender, SelectedItemChangedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
